d243e490255f4df4cceafe5bf3f3f1b3940636dd
Arpan <arpanseth04@gmail.com>
2025-07-23 02:56:59 +0530

    first commit

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ import string
 import nltk
 from nltk.corpus import stopwords
 from nltk.stem.porter import PorterStemmer
+nltk.download('punkt')
 
 # Download NLTK requirements
 nltk.download('punkt')
